651094e9d99b0b95ec66b27207e8959bdfd9b38b
ClaudiaThalita <claudiathalitamendes@outlook.com>
2023-04-23 15:32:31 -0300

    Testando a primiera classe de dominio

---
+++ Project-Spring/src/main/java/com/example/demo/domain/Categoria.java
@@ -0,0 +1,61 @@
+package com.example.demo.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+// Serializable é uma interface que diz que os obejtos dessa classe poderão ser convertidos em uma sequencia de bits 
+// Para que serve? Para que os objetos possam ser gravados em arquivos, para trafegar em rede
+public class Categoria implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String nome;
+	
+	public Categoria() {
+		
+	}
+
+	public Categoria(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	// gerando hash code equals, que vai comparar os objetos, neste caso, compararemos somente o id dos objetos	
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		return Objects.equals(id, other.id);
+	}
+//	
+	
+	
+}
